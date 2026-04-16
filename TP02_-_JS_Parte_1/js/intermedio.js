4822ff2373cfeee88a760a7ad3fb62d1c47a0777
Fran Hernández <fh.franhernandez.92@outlook.com>
2026-04-16 00:17:06 -0300

    Vinculando arreglo de 'objetos' de 'tipo tarea' en JS para insertar en el tbody de la tabla HTML de tareas

--- TP02_-_JS_Parte_1/js/intermedio.js
+++ TP02_-_JS_Parte_1/js/intermedio.js
@@ -1,4 +1,14 @@
 'use strict';
+let idTarea = 0;
+let tareas = [
+    crearTarea('asdasa', 'completar asdasa', 'completa'),
+    crearTarea('esdesde', 'completar esdesde',),
+    crearTarea('isdisdi', 'completar isdisdi',)
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+    refrescarTablaTareas();
+});
 
 document.getElementById('formParaMostrarNombreEnTitulo').addEventListener('submit', function(event) {
     ponerNombreEnTitulo(event);
@@ -12,5 +22,116 @@ function ponerNombreEnTitulo(event) {
         document.getElementById('mostrarNombreEnTitulo').innerHTML = nombre+' '+apellido;
     } else {
         document.getElementById('mostrarNombreEnTitulo').innerHTML = 'Mundo';
+    }
+}
+
+function crearTareaDeFormulario(idFormularioNuevaTarea) {
+    return {
+        id: ++idTarea,
+        nombre: document.forms[idFormularioNuevaTarea]['nombre'],
+        detalle: document.forms[idFormularioNuevaTarea]['detalle'],
+        estado: document.forms[idFormularioNuevaTarea]['estado']
+    };
+}
+function crearTarea(nombre, detalle = 'n/a', estado = 'incompleta') {
+    return {
+        id: ++idTarea,
+        nombre: nombre,
+        detalle: detalle,
+        estado: estado
+    };
+}
+
+function insertarTareaEnTabla(tarea, tablaID) {
+    // crear fila
+    let miTr = document.createElement('tr');
+
+    // crear el primer td
+    let miTd = document.createElement('td');
+    // inserta el contenido de tarea.id dentro del elemento td
+    miTd.innerHTML = tarea.id;
+    // lo inserta en la fila
+    miTr.appendChild(miTd);
+
+    // se reutiliza la variable para crear otro td en el árbol de contenido del DOM
+    miTd = document.createElement('td');
+    // inserta el contenido de tarea.nombre dentro del elemento td
+    miTd.innerHTML = tarea.nombre;
+    // lo inserta en la fila
+    miTr.appendChild(miTd);
+    
+    // se reutiliza la variable para crear otro td en el árbol de contenido del DOM
+    miTd = document.createElement('td');
+    // inserta el contenido de tarea.detalle dentro del elemento td
+    miTd.innerHTML = tarea.detalle;
+    // lo inserta en la fila
+    miTr.appendChild(miTd);
+
+        
+    // se reutiliza la variable para crear otro td en el árbol de contenido del DOM
+    miTd = document.createElement('td');
+    // inserta el contenido de tarea.estado dentro del elemento td
+    if(tarea.estado !== 'completa') {
+        miTd.innerHTML = '❕ ';
+    } else {
+        miTd.innerHTML = '✅ ';
+    }
+    miTd.innerHTML += convertirATitleCase(tarea.estado);
+    miTd.classList.add('columna-tarea-'+tarea.estado);
+
+    // lo inserta en la fila
+    miTr.appendChild(miTd);
+
+    // Última columna: botones de acción
+    miTd = document.createElement('td');
+    miTd.classList.add('columna-acciones');
+
+    // Primer botón: marcar completa/incompleta
+    let miBoton = document.createElement('button');
+    if(tarea.estado !== 'completa') {
+        miBoton.classList.add('btn-completar');
+        miBoton.title = 'Marcar Completa';
+        miBoton.innerHTML = '✅';
+    } else {
+        miBoton.classList.add('btn-incompletar');
+        miBoton.title = 'Marcar Incompleta';
+        miBoton.innerHTML = '❕';
+    }
+    // Agregar botón a la columna de acciones
+    miTd.appendChild(miBoton);
+
+    // Crear otro botón en el DOM usando la misma variable en memoria JS
+    miBoton = document.createElement('button');
+    miBoton.classList.add('btn-editar');
+    miBoton.title = 'Editar';
+    miBoton.innerHTML = '✏';
+    // Agregar botón a la columna de acciones
+    miTd.appendChild(miBoton);
+
+    
+    // Crear otro botón en el DOM usando la misma variable en memoria JS
+    miBoton = document.createElement('button');
+    miBoton.classList.add('btn-eliminar');
+    miBoton.title = 'Eliminar';
+    miBoton.innerHTML = '❌';
+    // Agregar botón a la columna de acciones
+    miTd.appendChild(miBoton);
+
+    // Agregar columna de botones a la fila
+    miTr.appendChild(miTd);
+
+    // Agregar un identificador para la fila
+    miTr.setAttribute('data-id-tarea', tarea.id);
+
+    // FINALMENTE se agrega la tr al body de la tabla
+    document.getElementById(tablaID).tBodies[0].appendChild(miTr);
+}
+
+function refrescarTablaTareas() {
+    document.getElementById('tabla-lista-tareas').tBodies[0].innerHTML = '';
+    if(tareas.length > 0) {
+        tareas.forEach((tarea) => {
+            insertarTareaEnTabla(tarea, 'tabla-lista-tareas');
+        });
     }
 }
--- TP02_-_JS_Parte_1/js/main.js
+++ TP02_-_JS_Parte_1/js/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function convertirATitleCase(str) {
+    if(str.length > 1)
+        return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+    if(str.length > 0)
+        return str.toUpperCase();
+    return null;
+}

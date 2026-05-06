96b4755aa9637510b50a3a97926c413a59f16663
Fran Hernández <fh.franhernandez.92@outlook.com>
2026-05-05 22:09:09 -0300

    Agregado captcha en página de contacto; TODO: hacer una tabla de lista de invitados y otras tablas necesarias

---
+++ TPE/js/contacto.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let captchas = [
+    {
+        codigo: "72581",
+        imagen: "captcha1.png"
+    },
+    {
+        codigo: "68473",
+        imagen: "captcha2.png"
+    },
+    {
+        codigo: "23591",
+        imagen: "captcha3.png"
+    },
+    {
+        codigo: "48639",
+        imagen: "captcha4.png"
+    },
+    {
+        codigo: "78265",
+        imagen: "captcha5.png"
+    }
+];
+let captchaActivo = null;
+let captchaResuelto = false;
+
+function cargarCaptchaAleatorio() {
+    captchaActivo = captchas[
+        Math.floor(
+            Math.random() * (captchas.length - 1)
+        )
+    ];
+    document.getElementById('imagen-captcha').setAttribute('src', 'img/captchas/'+captchaActivo.imagen);
+}
+
+document.addEventListener('DOMContentLoaded', cargarCaptchaAleatorio);
+
+document.getElementById('boton-captcha').addEventListener('click', function() {
+    let entrada = document.getElementById('input-captcha').value;
+    if(entrada !== captchaActivo.codigo) {
+        alert('Captcha incorrecto');
+        cargarCaptchaAleatorio();
+    }
+    else {
+        alert('Captcha correcto');
+        document.getElementById('captcha-contacto').remove();
+        document.getElementById('contenedor-contacto').style.display = 'block';
+    }
+});

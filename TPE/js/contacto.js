b7df7a3e6d4c5d65a76e2e8b0a077b10559da64f
FHFraHz <fh.franhernandez.92@outlook.com>
2026-05-10 04:28:43 -0300

    Agregado documentación a TPE

--- TPE/js/contacto.js
+++ TPE/js/contacto.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/*
+Arreglo de captchas:
+una "colección" interna de captchas que tiene JSON's
+dentro de los cuales se guarda el código real que tiene que ingresar el usuario
+    (un string, en este caso son números porque los dibujos son números pero puede ser cualquier cosa)
+y la imagen asociada a cada captcha.
+*/
 let captchas = [
     {
         codigo: "72581",
@@ -22,9 +29,20 @@ let captchas = [
         imagen: "captcha5.png"
     }
 ];
+
+/* El "captchaActivo" va a ser el que esté cargado en memoria
+"esperando" para después ser comparado con lo que ingrese el usuario */
 let captchaActivo = null;
 
 function cargarCaptchaAleatorio() {
+
+/*
+Math.floor(valor) es la función piso: se queda con la parte entera redondeando para abajo
+
+Math.random() genera un número aleatorio decimal entre 0 y 1,
+que luego se multiplicará por un máximo posible estipulado;
+en este caso el máximo posible debe ser EL TAMAÑO DEL ARREGLO DE CAPTCHAS MENOS 1.
+ */
     captchaActivo = captchas[
         Math.floor(
             Math.random() * (captchas.length - 1)
@@ -33,17 +51,27 @@ function cargarCaptchaAleatorio() {
     document.getElementById('imagen-captcha').setAttribute('src', 'img/captchas/'+captchaActivo.imagen);
 }
 
+/* Se pide que ni bien se cargue el DOM, se cargue un captcha aleatorio para mostrar
+y pedirle al usuario que lo ingrese */
 document.addEventListener('DOMContentLoaded', cargarCaptchaAleatorio);
 
-document.getElementById('boton-captcha').addEventListener('click', function() {
+/* Se le pide al botón de ID 'boton-captcha' que, al hacerle clic, llame a la función
+para validar el captcha con el valor que ingresó el usuario */
+document.getElementById('boton-captcha').addEventListener('click', validarCaptcha);
+
+function validarCaptcha() {
+    /* Al ser un input se debe traer su atributo value para pedir lo que ingresó el usuario */
     let entrada = document.getElementById('input-captcha').value;
+
+    /* Si es incorrecto, carga otro nuevo */
     if(entrada !== captchaActivo.codigo) {
         alert('Captcha incorrecto');
         cargarCaptchaAleatorio();
     }
+    /* Si es correcto, borra el contenido del captcha de la página y muestra el contenido oculto */
     else {
         alert('Captcha correcto');
         document.getElementById('captcha-contacto').remove();
         document.getElementById('contenedor-contacto').style.display = 'block';
     }
-});
+}
